6d43ca68c2ff82560df550b06a50acfe5bedb606
Syed Ashruf <ashrufsyed@gmail.com>
2020-01-10 15:48:50 +0530

    Added all the tested files to a newly created folder

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
+
+cd A_title
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
+
+cd ../B_toc
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
+
+cd ../C_chap
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
+
+cd ../D_appx
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
+
+cd ../E_end
+rm -rf *.acn *.aux *.bbl *.blg *.dvi *.glo *.glsdefs *.ist *.lof *.log *.lot *.nlg *.nlo *.nls *.out *.synctex.gz *.toc
